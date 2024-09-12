8a08895ce8b3cd6b09cd94fe62fcccacc8ca976d
aref-asadi <rf.aref.asadi@gmail.com>
2024-09-12 16:32:55 +0330

    Update and make website responsive better

--- src/medx_website/blog/urls.py
+++ src/medx_website/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import upload_image
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('upload_image/', upload_image, name='upload_image'),
 ]
