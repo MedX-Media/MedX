357c900abc9cb49d7e9a8cd6046a18576128ff52
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2024-10-07 06:45:39 +0000

    Test pre-commit hook

--- src/medx_website/blog/admin.py
+++ src/medx_website/blog/admin.py
@@ -1,20 +1,22 @@
 from django.contrib import admin
-from .models import Post, Tag, Category
-from .models import UserProfile
+
+from .models import Category, Post, Tag, UserProfile
+
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'profile_picture')
+    list_display = ("user", "profile_picture")
+
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publish_date', 'category', 'read_time')
-    list_filter = ('publish_date', 'author', 'tags', 'category')
-    search_fields = ('title', 'content', 'author__username')
-    prepopulated_fields = {'slug': ('title',)}
+    list_display = ("title", "author", "publish_date", "category", "read_time")
+    list_filter = ("publish_date", "author", "tags", "category")
+    search_fields = ("title", "content", "author__username")
+    prepopulated_fields = {"slug": ("title",)}
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
-        if request.user.groups.filter(name='نویسنده').exists():
+        if request.user.groups.filter(name="نویسنده").exists():
             return qs.filter(author=request.user)
         return qs
 
@@ -23,11 +25,14 @@ class PostAdmin(admin.ModelAdmin):
             obj.author = request.user
         super().save_model(request, obj, form, change)
 
+
 class TagAdmin(admin.ModelAdmin):
-    search_fields = ('name',)
+    search_fields = ("name",)
+
 
 class CategoryAdmin(admin.ModelAdmin):
-    search_fields = ('name',)
+    search_fields = ("name",)
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag, TagAdmin)
--- src/telegram_bot/main.py
+++ src/telegram_bot/main.py
@@ -1,25 +1,26 @@
-import re
-import requests
 import json
+import re
 import time
+
+import requests
 
 # Define the bot token and the channel chat ID for sending messages via Telegram
 BOT_TOKEN = "<BOT_TOKEN>"
 CHANNEL_CHAT_ID = "-1002215464363"
 
 # URL to access the GitHub API for the specific repository's commits and issues
-REPO_URL_API = 'https://api.github.com/repos/MedX-Media/MedX/commits'
-REPO_URL_ISSUES = 'https://api.github.com/repos/MedX-Media/MedX/issues'
+REPO_URL_API = "https://api.github.com/repos/MedX-Media/MedX/commits"
+REPO_URL_ISSUES = "https://api.github.com/repos/MedX-Media/MedX/issues"
 
 # Personal GitHub token for authentication
-GITHUB_TOKEN = '<GITHUB_TOKEN>'
+GITHUB_TOKEN = "<GITHUB_TOKEN>"
 
 
 def recive_last_data(api_url, github_token):
     headers = {
-        'Accept': 'application/vnd.github+json',
-        'Authorization': f'Bearer {github_token}',
-        'X-GitHub-Api-Version': '2022-11-28',
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {github_token}",
+        "X-GitHub-Api-Version": "2022-11-28",
     }
     response = requests.get(api_url, headers=headers)
     data = response.json()
@@ -31,7 +32,7 @@ def recive_last_data(api_url, github_token):
 
 
 def save_json_to_file(data, filename):
-    with open(filename, 'w') as json_file:
+    with open(filename, "w") as json_file:
         json.dump(data, json_file, indent=4)
 
 
@@ -41,7 +42,7 @@ def extract_commit_info(commit_data):
         "author_name": commit_data["commit"]["author"].get("name"),
         "author_email": commit_data["commit"]["author"].get("email"),
         "message": commit_data["commit"].get("message"),
-        "commit_url": commit_data.get("html_url")
+        "commit_url": commit_data.get("html_url"),
     }
     return important_info
 
@@ -52,52 +53,54 @@ def extract_issue_info(issue_data):
         "title": issue_data.get("title"),
         "user_login": issue_data["user"].get("login"),
         "state": issue_data.get("state"),
-        "labels":
-        [label.get("name") for label in issue_data.get("labels", [])],
+        "labels": [label.get("name") for label in issue_data.get("labels", [])],
         "created_at": issue_data.get("created_at"),
         "updated_at": issue_data.get("updated_at"),
         "issue_url": issue_data.get("html_url"),
-        "body": issue_data.get("body", "")
+        "body": issue_data.get("body", ""),
     }
     return important_info
 
 
 def escape_markdown(text):
-    escape_chars = r'\*_`\[\]()~>#+-=|{}.!'
-    return re.sub(r'([%s])' % re.escape(escape_chars), r'\\\1', text)
+    escape_chars = r"\*_`\[\]()~>#+-=|{}.!"
+    return re.sub(r"([%s])" % re.escape(escape_chars), r"\\\1", text)
 
 
 def format_commit_message(info):
-    info['message'] = escape_markdown(info['message'])
-    message = ("🚀 *New Commit Detected*\n\n"
-               "🔗 *SHA:* `{sha}`\n"
-               "👤 *Author:* {author_name} ({author_email})\n"
-               "📝 *Commit Message:* {message}\n\n"
-               "🔍 [View Commit]({commit_url})".format(**info))
+    info["message"] = escape_markdown(info["message"])
+    message = (
+        "🚀 *New Commit Detected*\n\n"
+        "🔗 *SHA:* `{sha}`\n"
+        "👤 *Author:* {author_name} ({author_email})\n"
+        "📝 *Commit Message:* {message}\n\n"
+        "🔍 [View Commit]({commit_url})".format(**info)
+    )
     return message
 
 
 def format_issue_message(info):
-    info['body'] = escape_markdown(info['body'])
+    info["body"] = escape_markdown(info["body"])
     labels = ", ".join([escape_markdown(label) for label in info["labels"]])
-    message = (f"🐞 *New Issue Detected* - "
-               f"🔢 *Issue Number:* `{info['issue_number']}`\n\n"
-               f"📝 *Title:* {info['title']}\n"
-               f"👤 *User:* {info['user_login']}\n"
-               f"🏷️ *Labels:* {labels}\n\n"
-               f"📅 *Created At:* {info['created_at']}\n"
-               f"🔄 *Updated At:* {info['updated_at']}\n\n"
-               f"🔗 [View Issue]({info['issue_url']})\n\n")
+    message = (
+        f"🐞 *New Issue Detected* - "
+        f"🔢 *Issue Number:* `{info['issue_number']}`\n\n"
+        f"📝 *Title:* {info['title']}\n"
+        f"👤 *User:* {info['user_login']}\n"
+        f"🏷️ *Labels:* {labels}\n\n"
+        f"📅 *Created At:* {info['created_at']}\n"
+        f"🔄 *Updated At:* {info['updated_at']}\n\n"
+        f"🔗 [View Issue]({info['issue_url']})\n\n"
+    )
     return message
 
 
 def send_tel(mess, bot_token, chat_id):
-    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     data = {
-        'chat_id': chat_id,
-        'text': mess,
-        'parse_mode':
-        'Markdown'  # Use 'MarkdownV2' if necessary and ensure proper escaping
+        "chat_id": chat_id,
+        "text": mess,
+        "parse_mode": "Markdown",  # Use 'MarkdownV2' if necessary and ensure proper escaping
     }
     response = requests.post(url, data=data)
     return response
@@ -110,9 +113,9 @@ last_checked_issue_number = None
 while True:
     # Check for new commits
     last_commit_data = recive_last_data(REPO_URL_API, GITHUB_TOKEN)
-    if last_commit_data and last_commit_data['sha'] != last_checked_sha:
-        last_checked_sha = last_commit_data['sha']
-        save_json_to_file(last_commit_data, 'commit_data.json')
+    if last_commit_data and last_commit_data["sha"] != last_checked_sha:
+        last_checked_sha = last_commit_data["sha"]
+        save_json_to_file(last_commit_data, "commit_data.json")
         print(f"New commit detected and data saved to 'commit_data.json'")
         commit_info = extract_commit_info(last_commit_data)
         telegram_message = format_commit_message(commit_info)
@@ -124,10 +127,9 @@ while True:
 
     # Check for new issues
     last_issue_data = recive_last_data(REPO_URL_ISSUES, GITHUB_TOKEN)
-    if last_issue_data and last_issue_data[
-            'number'] != last_checked_issue_number:
-        last_checked_issue_number = last_issue_data['number']
-        save_json_to_file(last_issue_data, 'issue_data.json')
+    if last_issue_data and last_issue_data["number"] != last_checked_issue_number:
+        last_checked_issue_number = last_issue_data["number"]
+        save_json_to_file(last_issue_data, "issue_data.json")
         print(f"New issue detected and data saved to 'issue_data.json'")
         issue_info = extract_issue_info(last_issue_data)
         telegram_message = format_issue_message(issue_info)
@@ -139,4 +141,3 @@ while True:
 
     # Wait for a specific amount of time before checking again (e.g., 60 seconds)
     time.sleep(60)
-
