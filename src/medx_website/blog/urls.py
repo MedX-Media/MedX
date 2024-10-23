54cd0ab0a53a47976d8439747979afc87f9086f0
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2024-10-23 15:51:53 +0000

    Update urls.py - add "search/" url

--- src/medx_website/blog/urls.py
+++ src/medx_website/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import upload_image
+from .views import upload_image, search_posts
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
-    path('upload_image/', upload_image, name='upload_image'),
+    path("", views.home, name="home"),
+    path("post/<slug:slug>/", views.post_detail, name="post_detail"),
+    path("search/", search_posts, name="search_posts"),
+    path("upload_image/", upload_image, name="upload_image"),
 ]
